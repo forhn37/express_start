8b487377fc161844aef4807f39895cacfa9d000f
kimhyun <forhn37@gmail.com>
2023-11-14 14:58:50 +0900

    템플릿 언어로 서버 동적처리 완료 다음에 진행

--- meadowlark.js
+++ meadowlark.js
@@ -12,12 +12,22 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars')
 
+
+// 포춘쿠키 추가
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+]
 app.get('/', (req, res) => {
   res.render('home')
 })
 
 app.get('/about', (req, res) => {
-  res.render("about")
+  const randomFourtune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render("about", {fortune:randomFourtune})
 })
 
 // custom 404 page
@@ -38,6 +48,7 @@ app.listen(port, () => {console.log(
   `press Ctrl-C to terminate.`
 )
 })
+
 
 // 미들웨어등록시 app.use를 하면 모든 곳에 적용된다. 
 // 그래서 미들웨어가 위에서 모든 엔드포인트를 적용하면 뒤에있는 엔드포인트에 적용이 안된다. 주의!!
