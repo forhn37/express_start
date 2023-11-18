027c480dd23d2d83ea50fdad6d02fbc1600ca554
kimhyun <forhn37@gmail.com>
2023-11-18 15:09:30 +0900

    feat : 라우터파일 생성

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const expressHandlebars = require('express-handlebars')
 const app = express();
+const handlers = require('./lib/handlers')
 const fortune =require('./lib/fortune')
 const port = process.env.PORT || 3000;
 
@@ -14,26 +15,15 @@ app.engine('handlebars', expressHandlebars({
 app.set('view engine', 'handlebars')
 
 // home, about 렌더링 설정
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req,res) => {
-  res.render('about', { fortune :fortune.getFortune()})
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req,res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err,req,res,next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => {console.log(
   `Express Started on http://localhost:${port}; ` +
